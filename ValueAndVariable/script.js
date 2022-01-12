7230b3c46d4a8e4c4232dc6e7a107631f67d1794
Ranjan <shashi.b.ranjan@capgemini.com>
2022-01-13 00:07:40 +0530

    updated

--- ValueAndVariable/script.js
+++ ValueAndVariable/script.js
@@ -1,2 +1,10 @@
 let firstName="Shashi Ranjan";
 console.log(firstName);
+
+const now=2037;
+const ageJohn=now-1991;
+const ageSearch=now-2018;
+console.log(ageJohn,ageSearch);
+
+console.log(ageJohn*2, ageJohn/10,2**5);
+
